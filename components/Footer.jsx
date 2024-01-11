774c0668b8010b3d688a1259e000f812269a1906
yash-yj10 <yj371525@gmail.com>
2024-01-12 02:37:05 +0530

    .

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -110,7 +110,7 @@ const Footer = () => {
             <Wrapper className="flex justify-between mt-10 flex-col md:flex-row gap-[10px] md:gap-0">
                 {/* LEFT START */}
                 <div className="text-[12px] text-white/[0.5] hover:text-white cursor-pointer text-center md:text-left">
-                    © 2023 SHOE STORE, Inc. All Rights Reserved
+                    © 2024 SHOE STORE, Inc. All Rights Reserved
                 </div>
                 {/* LEFT END */}
 
